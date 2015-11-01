71db42252efc035ddda00e93ab2d11bde88ff68e
TeeNak <teenak77@gmail.com>
2015-11-01 22:51:35 +0900

    added ui using web service

---
+++ hrdemo-ui/app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hrdemoUiApp
+ * @description
+ * # hrdemoUiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hrdemoUiApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.grid',
+    'ui.grid.edit',
+    'pascalprecht.translate'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ hrdemo-ui/app/scripts/controllers/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hrdemoUiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hrdemoUiApp
+ */
+angular.module('hrdemoUiApp')
+.controller('MainCtrl', ['$scope','i18nService','$http', 
+        function($scope,i18nService,$http) {
+    $scope.langs = i18nService.getAllLangs();
+    $scope.lang = 'ja';
+
+    $scope.gridOptions = { 
+        columnDefs: [
+            { displayName: 'CODE', field: 'code', headerCellFilter: 'translate'},
+            { displayName: 'NAME', field: 'name', headerCellFilter: 'translate'},
+            { displayName: 'VERSION', field: 'version', headerCellFilter: 'translate', enableFiltering: false }
+        ] 
+    };
+
+    $http.get('/hrdemo/jobs')
+    .success(function(jsonData){
+        $scope.gridOptions.data = jsonData._embedded.jobs;    
+    });
+
+}])
+.config(function($translateProvider) {
+    $translateProvider.translations('en', {
+        CODE: 'Code',
+        NAME: 'Name',
+        VERSION: 'Version'
+    });
+    $translateProvider.translations('ja', {
+        CODE: 'コード',
+        NAME: '名前',
+        VERSION: 'バージョン'
+    });
+    $translateProvider.preferredLanguage('ja'); 
+    $translateProvider.useSanitizeValueStrategy('escape');
+});
+
+
